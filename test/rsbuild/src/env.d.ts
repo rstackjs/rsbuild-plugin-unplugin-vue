1548aabc30457111549bbf1a8a2e06fde4e4d353
neverland <chenjiahan.jait@bytedance.com>
2026-04-28 21:28:00 +0800

    chore: migrate to rslint and prettier

--- test/rsbuild/src/env.d.ts
+++ test/rsbuild/src/env.d.ts
@@ -3,8 +3,6 @@
 declare module '*.vue' {
   import type { DefineComponent } from 'vue';
 
-  // biome-ignore lint/complexity/noBannedTypes: reason
-  // biome-ignore lint/suspicious/noExplicitAny: Vue component typings use `any` here.
-  const component: DefineComponent<{}, {}, any>;
+  const component: DefineComponent;
   export default component;
 }
